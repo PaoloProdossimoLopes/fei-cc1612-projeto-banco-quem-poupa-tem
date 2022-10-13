9328cd42e5cdbe7752cc35edc4facd6f75e632de
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-10-13 19:32:14 -0300

    Chore (main): Imprimindo menu de opçoes

---
+++ src/main.py
@@ -0,0 +1,12 @@
+def run():
+    while True:
+        print('''
+        1 - Novo cliente
+        2 - Apagar cliente
+        3 - Débito
+        4 - Depósito
+        5 - Extrato
+        6 - Transferencia entre contas
+        7 - 'Operação livre'
+        8 - Sair
+        ''')
