a937982e9284a0d8a1cebe4263abfdb46dfec296
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-10-13 19:42:37 -0300

    Refactor (main): Extraindo codigos verbosos ou repetitivos em metodos

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
-def run():
-    while True:
-        print('''
+def alerta_de_opcao_invalida():
+    print('Opção invalida! selecione uma opcao valida para continuar.')    
+
+def imprime_menu():
+    print('''
         OPÇOES DO BANCO:
         1 - Novo cliente
         2 - Apagar cliente
@@ -10,10 +12,18 @@ def run():
         6 - Transferencia entre contas
         7 - 'Operação livre'
         8 - Sair
-        ''')
+    ''')
+
+def run():
+    while True:
+        imprime_menu()
 
         try:
             opcao = int(input('Oque deseja realizar no banco? '))
+            if opcao in range(1, 8):
+                pass
+            else:
+                alerta_de_opcao_invalida()
 
         except:
-            print('Opção invalida! selecione uma opcao valida para continuar.')
+            alerta_de_opcao_invalida()
