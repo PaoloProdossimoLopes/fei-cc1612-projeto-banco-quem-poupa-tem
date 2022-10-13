7af261ca24d99ecfb132415e5fd31f8ecdfd99e7
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-10-13 19:37:06 -0300

    Chore (main): Recebendo e tratando a entrada

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 def run():
     while True:
         print('''
+        OPÇOES DO BANCO:
         1 - Novo cliente
         2 - Apagar cliente
         3 - Débito
@@ -10,3 +11,9 @@ def run():
         7 - 'Operação livre'
         8 - Sair
         ''')
+
+        try:
+            opcao = int(input('Oque deseja realizar no banco? '))
+
+        except:
+            print('Opção invalida! selecione uma opcao valida para continuar.')
