587931f722add67ec26cca620717771009b71d6f
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-10-13 23:44:00 -0300

    Refactor (main): Criando helpers e evitando magic numbers

--- src/main.py
+++ src/main.py
@@ -1,67 +1,101 @@
-def alerta_de_opcao_invalida():
-    print('Opção invalida! selecione uma opcao valida para continuar.')    
-
 def imprime_menu():
-    print('''
-        OPÇOES DO BANCO:
-        1 - Novo cliente
-        2 - Apagar cliente
-        3 - Débito
-        4 - Depósito
-        5 - Extrato
-        6 - Transferencia entre contas
-        7 - 'Operação livre'
-        8 - Sair
-    ''')
+    header = '''
+    ********************************
+    Serviços:
+    1 - Novo cliente
+    2 - Apagar cliente
+    3 - Débito
+    4 - Depósito
+    5 - Extrato
+    6 - Transferencia entre contas
+    7 - 'Operação livre'
+    0 - Sair
+    ********************************
+    '''
+    print(header)
+
+def validando_valor(mensagem):
+    try:
+        valor = float(input(mensagem + ' '))
+        return valor
+    except:
+        print('❌ Valor invalido! Coloque um valor valido')
+        validando_valor(mensagem)
+
+
+def validando_tipo_conta():
+    tipo_conta = input('Tipo de conta: ')
+    tipo_conta = tipo_conta.lower()
+
+    if tipo_conta == 'comum' or tipo_conta == 'plus':
+        return tipo_conta
+    
+    else:
+        print('❌ Valor invalido! Escolha um tipo de conta valido (comum ou plus)')
+        validando_tipo_conta()
+
 
 def executar_opcao_novo_cliente():
     nome = input('Nome: ')
     cpf = input('CPF: ')
-    tipo_conta = input('Tipo de conta: ')
-    valor_inicial = input('Valor inicial da conta: ')
-    print('Nome:', nome)
-    print('CPF:', cpf)
-    print('Tipo de conta:', tipo_conta)
-    print('Valor inicial:', valor_inicial)
+    tipo_conta = validando_tipo_conta()
+    valor_inicial = validando_valor('Valor inicial da conta:')
+
 
 def executar_opcao_deletando_cliente():
     pass
 
+
 def executar_opcao_debito():
     cpf = input('CPF: ')
     senha = input('Senha: ')
-    valor = float(input('Valor: '))
-    print('CPF:', cpf)
-    print('Senha:', senha)
-    print('Valor:', valor)
+    valor = validando_valor('Valor:')
+
 
 def executar_opcao_deposito():
     cpf = input('CPF: ')
-    valor = float(input('Valor: '))
-    print("CPF", cpf)
-    print("Valor", valor)
+    valor = valor = validando_valor('Valor:')
+
 
 def executar_opcao_extrato():
     cpf = input('CPF: ')
     senha = input('Senha: ')
-    print('Valor:', valor)
-    print('Senha:', senha)
+
 
 def executar_opcao_transferencia():
     origem_cpf = input('CPF (Origem): ')
     origem_senha = input('Senha (Origem): ')
     destino_cpf = input('CPF (Destino): ')
-    valor = input('Valor: ')
-    print('CPF (Origem):', origem_cpf)
-    print('Senha (Origem):', origem_senha)
-    print('CPF (Destino):', destino_cpf)
-    print('Valor:', valor)
+    valor = validando_valor('Valor:')
+
 
 def executar_opcao_livre():
     pass
 
+
 def executar_opcao_sair():
-    print('Obrigado por usar nosos serviços!')
+    print('👋🏼 Obrigado por usar nosos serviços! 👋🏼')
+
+def eh_novo_cliente(opcao):
+    return opcao == 1
+
+def eh_deletat_cliente(opcao):
+    return opcao == 2
+
+def eh_debito(opcao):
+    return opcao == 3
+
+def eh_deposito(opcao):
+    return opcao == 4
+
+def eh_extrato(opcao):
+    return opcao == 5
+
+def eh_transferencia(opcao):
+    return opcao == 6
+
+def eh_opcao_livre(opcao):
+    return opcao == 7
 
 def run():
     while True:
@@ -69,36 +103,39 @@ def run():
 
         try:
             opcao = int(input('Oque deseja realizar no banco? '))
-            if opcao in range(0, 8):
-                if (opcao == 1):
+
+            OPCAO_MIN = 0
+            OPCAO_MAX = 8
+            if opcao in range(OPCAO_MIN, OPCAO_MAX):
+                if eh_novo_cliente(opcao):
                     executar_opcao_novo_cliente()
 
-                elif (opcao == 2):
+                elif eh_deletat_cliente(opcao):
                     executar_opcao_deletando_cliente()
 
-                elif (opcao == 3):
+                elif eh_debito(opcao):
                     executar_opcao_debito()
 
-                elif (opcao == 4):
+                elif eh_deposito(opcao):
                     executar_opcao_deposito()
 
-                elif (opcao == 5):
+                elif eh_extrato(opcao):
                     executar_opcao_extrato()
 
-                elif (opcao == 6):
+                elif eh_transferencia(opcao):
                     executar_opcao_transferencia()
 
-                elif (opcao == 7):
+                elif eh_opcao_livre(opcao):
                     executar_opcao_livre()
 
                 else:
                     executar_opcao_sair()
                     break
             else:
-                alerta_de_opcao_invalida()
+                print('⚠️ Opção invalida! Escolha um numero entre 0 - 7.')   
 
         except:
-            alerta_de_opcao_invalida()
+            print('⚠️ Opção invalida! Escolha apenas numeros.')   
 
 
 if __name__ == '__main__':
