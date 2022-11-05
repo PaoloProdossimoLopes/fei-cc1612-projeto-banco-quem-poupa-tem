b45b3b4d8db565da3f2a2b028b08d88031c97385
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-11-05 20:47:45 -0300

    refactor: move use cases helpers to seprated file

---
+++ src/useCaseHelpers.py
@@ -0,0 +1,89 @@
+import datetime
+
+import Constant
+import fileManager
+
+def cadastrarCliente(cpf, json_dict):
+    dic = {}
+    try:
+        dic = fileManager.load()
+    except:
+        fileManager.save(json_dict)
+        main.run()
+
+    dic[cpf] = json_dict[cpf]
+
+    fileManager.save(dic)
+
+def criaClienteDict(cpf, nome, tipo, valor, senha):
+    cliente = dict()
+
+    cliente[Constant.NAME_KEY] = nome
+    cliente[Constant.TYPE_KEY] = tipo
+    cliente[Constant.VALUE_KEY] = valor
+    cliente[Constant.PASSWORD_KEY] = senha
+    cliente[Constant.EVENTS_KEYS] = []
+
+    cliente_informacao = dict()
+    cliente_informacao[cpf] = cliente
+
+    return cliente_informacao
+
+def registarar_evento(cpf, valor):
+    dic = {}
+    try:
+        dic = fileManager.load()
+    except:
+        print(Constant.ERROR_ON_REGISTER_EVENT_MESSAGE)
+        main.run()
+        return
+
+    literal_tarifa = dic[cpf][Constant.TYPE_KEY]
+    tarifa = chose_tax(literal_tarifa, valor)
+
+    saldo = dic[cpf][Constant.VALUE_KEY]
+    eventos = dic[cpf][Constant.EVENTS_KEYS]
+    today = datetime.datetime.now()
+
+    indicator = chose_indicator(valor)
+    valor = abs(valor)
+
+    lancamento = f'Data: {today.year}-{today.month}-{today.day} {today.hour}:{today.minute}:{today.second} {indicator} {valor} Tarifa: {tarifa} Saldo: {saldo}'
+    eventos.append(lancamento)
+    dic[cpf][Constant.EVENTS_KEYS] = eventos
+
+    fileManager.save(dic)
+
+
+def chose_tax(literal, value):
+
+    is_debit_operation = value < 0
+    if literal == Constant.COMMON_TYPE_LITERAL and is_debit_operation:
+        return value * Constant.COMMON_TAX
+    elif literal == Constant.PLUS_TYPE_LITERAL and is_debit_operation:
+        return value * Constant.PLUS_TAX
+    else:
+        return 0.0
+
+def chose_indicator(value):
+    if value > 0:
+        return Constant.POSITIVE_SYMBOL
+    else:
+        return Constant.NEGATIVE_SYMBOL
+
+def debitar(cpf, senha, valor):
+    dict_json = fileManager.load()
+
+    if dict_json[cpf][Constant.PASSWORD_KEY] == senha:
+        dict_json[cpf][Constant.VALUE_KEY] -= valor
+        fileManager.save(dict_json)
+        
+    else:
+        print(Constant.DATA_INVALID_TRY_AGAIN_ERROR_MESSAGE)
+        executar_opcao_debito()
+        return
+    
+def depositar(cpf, valor):
+    dict_json = fileManager.load()
+    dict_json[cpf][Constant.VALUE_KEY] += valor
+    fileManager.save(dict_json)
--- src/useCases.py
+++ src/useCases.py
@@ -1,8 +1,7 @@
-import datetime
-
 import validator
 import fileManager
 import Constant
+import useCaseHelpers as useCaseHelper
 import main
 
 
@@ -12,38 +11,12 @@ def executar_opcao_novo_cliente():
     senha = input(Constant.PASSWORD_PLACEHOLDER)
     tipo_conta = validator.validando_tipo_conta()
     valor_inicial = validator.validando_valor(Constant.INITIAL_ACCOUNT_VALUE)
-    json = criaClienteDict(cpf, nome, tipo_conta, valor_inicial, senha)
-    cadastrarCliente(cpf, json)
-
-def cadastrarCliente(cpf, json_dict):
-    dic = {}
-    try:
-        dic = fileManager.load()
-    except:
-        fileManager.save(json_dict)
-        main.run()
-
-    dic[cpf] = json_dict[cpf]
-
-    fileManager.save(dic)
+    json = useCaseHelper.criaClienteDict(cpf, nome, tipo_conta, valor_inicial, senha)
+    useCaseHelper.cadastrarCliente(cpf, json)
 
 def cliente_ainda_nao_esta_registardo(cpf, clientes_registrados):
     cpfs = clientes_registrados.keys()
     return (cpf in cpfs) == False
-
-def criaClienteDict(cpf, nome, tipo, valor, senha):
-    cliente = dict()
-
-    cliente[Constant.NAME_KEY] = nome
-    cliente[Constant.TYPE_KEY] = tipo
-    cliente[Constant.VALUE_KEY] = valor
-    cliente[Constant.PASSWORD_KEY] = senha
-    cliente[Constant.EVENTS_KEYS] = []
-
-    cliente_informacao = dict()
-    cliente_informacao[cpf] = cliente
-
-    return cliente_informacao
 
 def executar_opcao_deletando_cliente():
     cpf = input(Constant.CPF_PLACEHOLDER)
@@ -59,70 +32,16 @@ def executar_opcao_debito():
     senha = input(Constant.PASSWORD_PLACEHOLDER)
     valor = validator.validando_valor(Constant.VALUE_PLACEHOLDER)
 
-    debitar(cpf, senha, valor)
-    registarar_evento(cpf, valor)
+    useCaseHelper.debitar(cpf, senha, valor)
+    useCaseHelper.registarar_evento(cpf, -valor)
 
-def registarar_evento(cpf, valor):
-    dic = {}
-    try:
-        dic = fileManager.load()
-    except:
-        print(Constant.ERROR_ON_REGISTER_EVENT_MESSAGE)
-        main.run()
-        return
-
-    literal_tarifa = dic[cpf][Constant.TYPE_KEY]
-    tarifa = chose_tax(literal_tarifa, valor)
-
-    saldo = dic[cpf][Constant.VALUE_KEY]
-    eventos = dic[cpf][Constant.EVENTS_KEYS]
-    today = datetime.datetime.now()
-
-    indicator = chose_indicator(valor)
-    valor = abs(valor)
-
-    lancamento = f'Data: {today.year}-{today.month}-{today.day} {today.hour}:{today.minute}:{today.second} {indicator} {valor} Tarifa: {tarifa} Saldo: {saldo}'
-    eventos.append(lancamento)
-    dic[cpf][Constant.EVENTS_KEYS] = eventos
-
-    fileManager.save(dic)
-
-
-def chose_tax(literal, value):
-
-    is_debit_operation = value < 0
-    if literal == Constant.COMMON_TYPE_LITERAL and is_debit_operation:
-        return value * Constant.COMMON_TAX
-    elif literal == Constant.PLUS_TYPE_LITERAL and is_debit_operation:
-        return value * Constant.PLUS_TAX
-    else:
-        return 0.0
-
-def chose_indicator(value):
-    if value > 0:
-        return Constant.POSITIVE_SYMBOL
-    else:
-        return Constant.NEGATIVE_SYMBOL
-
-def debitar(cpf, senha, valor):
-    dict_json = fileManager.load()
-
-    if dict_json[cpf][Constant.PASSWORD_KEY] == senha:
-        dict_json[cpf][Constant.VALUE_KEY] -= valor
-        fileManager.save(dict_json)
-        
-    else:
-        print(Constant.DATA_INVALID_TRY_AGAIN_ERROR_MESSAGE)
-        executar_opcao_debito()
-        return
-    
 
 def executar_opcao_deposito():
     cpf = input(Constant.CPF_PLACEHOLDER)
     valor = validator.validando_valor(Constant.VALUE_PLACEHOLDER)
 
-    depositar(cpf, valor)
-    registarar_evento(cpf, valor)
+    useCaseHelper.depositar(cpf, valor)
+    useCaseHelper.registarar_evento(cpf, valor)
 
 
 def executar_opcao_extrato():
@@ -146,20 +65,15 @@ def executar_opcao_transferencia():
     valor = validator.validando_valor(Constant.VALUE_PLACEHOLDER)
 
     try:
-        debitar(origem_cpf, origem_senha, valor)
-        depositar(destino_cpf, valor)
+        useCaseHelper.debitar(origem_cpf, origem_senha, valor)
+        useCaseHelper.depositar(destino_cpf, valor)
 
-        registarar_evento(origem_cpf, -valor)
-        registarar_evento(destino_cpf, valor)
+        useCaseHelper.registarar_evento(origem_cpf, -valor)
+        useCaseHelper.registarar_evento(destino_cpf, valor)
     except:
         print(Constant.OCCOUR_AN_ERROR_MESSAGE)
         executar_opcao_transferencia()
 
-    
-def depositar(cpf, valor):
-    dict_json = fileManager.load()
-    dict_json[cpf][Constant.VALUE_KEY] += valor
-    fileManager.save(dict_json)
 
 def executar_opcao_livre():
     pass
